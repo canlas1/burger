4efff2554dbf32e5b0fec79b5319c47b8a3175c6
canlas1 <dc@uscr.com>
2017-05-15 11:54:01 -0400

    Gotta get some pupusas

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
+  host: "ls54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "root",
-  password: "",
+  password: "tta4vta18poyv4ll",
   database: "burgers_db"
 });
 
